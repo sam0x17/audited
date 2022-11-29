5237ad7500e754be00dbfa1207a8332b79079a2d
Sam Johnson <sam@durosoft.com>
2022-11-28 23:29:53 -0500

    macro scaffold

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,1 +1,9 @@
+use proc_macro::TokenStream;
 
+extern crate quote;
+extern crate syn;
+
+#[proc_macro_attribute]
+pub fn audited(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    item
+}
